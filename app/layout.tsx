08f9cf2288c7a4e308e21eb94bbb7140223e0d6d
umeriftikhar391-sketch <umer.iftikhar391@gmail.com>
2026-09-25 00:55:05 +0500

    Added Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import Script from "next/script";
+
 import SmoothScroll from "@/components/SmoothScroll";
 import Background from "@/components/Background";
 
@@ -35,11 +37,89 @@ return (
 <body className="bg-black text-white">
 
 
+{/* Google Tag Manager */}
+
+<noscript>
+
+<iframe
+
+src="https://www.googletagmanager.com/ns.html?id=GTM-59B6S87"
+
+height="0"
+
+width="0"
+
+style={{
+
+display:"none",
+
+visibility:"hidden"
+
+}}
+
+/>
+
+</noscript>
+
+
+
+<Script
+
+id="google-tag-manager"
+
+strategy="afterInteractive"
+
+>
+
+{`
+
+(function(w,d,s,l,i){
+
+w[l]=w[l]||[];
+
+w[l].push({
+
+'gtm.start':
+
+new Date().getTime(),
+
+event:'gtm.js'
+
+});
+
+var f=d.getElementsByTagName(s)[0],
+
+j=d.createElement(s),
+
+dl=l!='dataLayer'?'&l='+l:'';
+
+j.async=true;
+
+j.src=
+
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+f.parentNode.insertBefore(j,f);
+
+})(window,document,'script','dataLayer','GTM-59B6S87');
+
+`}
+
+</Script>
+
+
+
 {/* Animated Background */}
 
 <Background />
 
 
+
+
+
+{/* Smooth Scroll */}
+
+<SmoothScroll />
 
 
 
